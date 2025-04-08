24863df5f085269068cfddb4b04a4c5c41538233
ogar_m <ogar.m.v@mail.ru>
2025-04-08 10:33:26 +0300

    smart house

--- src/main/java/ElectricDevice.java
+++ src/main/java/ElectricDevice.java
@@ -1,5 +1,17 @@
 public interface ElectricDevice {
+    boolean isSwitched();
+
     void switchOn();
+
     void switchOff();
+
     int getEnergyConsumption();
+
+    default void toggle() {
+        if (isSwitched()) {
+            switchOff();
+        } else {
+            switchOn();
+        }
+    }
 }
